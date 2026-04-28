487d2f4380683ffe18b89678992ebd680684373c
Barla Fam <nd4zmoe@gmail.com>
2026-04-28 07:06:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * SERVICE WORKER keuanganKU (BASIC - 100% OFFLINE)
- * Versi 1.01
+ * Versi 1.02
  */
 
-const APP_VERSION = '1.01'; 
+const APP_VERSION = '1.02'; 
 const CACHE_PREFIX = 'keuanganku-basic-';
 const CACHE_STATIC = CACHE_PREFIX + 'static-v' + APP_VERSION;
 const CACHE_DYNAMIC = CACHE_PREFIX + 'dynamic-v' + APP_VERSION;
